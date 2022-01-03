466a0ad10798a6f2e8b4c502ed971f4716311ed2
Blok5 <Simakoff10@mail.ru>
2022-01-03 16:06:30 +0300

    1) Coin Change 2) ConsecutiveUnits

---
+++ src/algorithms/ConsecutiveUnits.java
@@ -0,0 +1,42 @@
+package algorithms;
+
+/**
+ * Требуется найти в бинарном векторе самую длинную последовательность единиц и вывести её длину.
+ */
+public class ConsecutiveUnits {
+    public static void main(String[] args) {
+        int[] n = {0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1};
+        System.out.println(countIt(n));
+        System.out.println(countRec(n, 0, n.length, 0));
+    }
+
+    /**
+     * Итеративное решение
+     */
+    public static int countIt(int[] s) {
+        int cur = 0, max = 0;
+        for (int i = 0; i < s.length; i++) {
+            if (s[i] == 1) {
+                cur++;
+                if (cur > max) max = cur;
+            } else cur = 0;
+        }
+        return max;
+    }
+
+    /**
+     * Рекурсивное решение
+     */
+    public static int countRec(int[] s, int count, int sLength, int max) {
+        if (sLength == 0) return max;
+        if (s[sLength - 1] == 1) {
+            count++;
+            if (count > max) max = count;
+        } else {
+            count = 0;
+        }
+
+        return countRec(s, count, sLength -= 1, max);
+
+    }
+}
