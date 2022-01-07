e4895704b6e6c4233a6d3040525b0d6dc216ad0f
Blok5 <Simakoff10@mail.ru>
2022-01-07 14:50:54 +0300

    QuickSort NormalizeString

---
+++ src/algorithms/NormalizeString.java
@@ -0,0 +1,33 @@
+package algorithms;
+
+/**
+ * You are given a string -> normalize it
+ */
+public class NormalizeString {
+    public static void main(String[] args) {
+        System.out.println(normWithRegex("good       string                   "));
+    }
+
+    public static String norm(String s) {
+        boolean first = true;
+        String res = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == ' ') {
+                if (first) {
+                    res += c;
+                    first = false;
+                }
+            } else {
+                res += c;
+                first = true;
+            }
+        }
+
+        return res.trim();
+    }
+    public static String normWithRegex(String s) {
+        return s.replaceAll("\\s+", " ").trim();
+    }
+}
